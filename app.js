1006b4e72b03f00c82dcf56360c6c42af7f3102c
Arthittapoom <118807101+Arthittapoom@users.noreply.github.com>
2023-05-10 11:29:16 +0700

    add all files
    ok

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) =>{
+
+    res.send('hello borntoDev');
+
+})
+
+app.listen(port, ()=>{
+    debug("Lisrening on port " + chalk.green(port));
+})
